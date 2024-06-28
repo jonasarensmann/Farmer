a494bcfd8dfca25a0dafc586772a1b93a87c6e98
Jonas <jonas@jonas-arensmann.com>
2024-06-28 16:19:54 +0200

    fix

--- web/db/init.sql
+++ web/db/init.sql
@@ -14,8 +14,8 @@ DEFINE SCOPE users SESSION 24h
 
 DEFINE TABLE crops SCHEMAFULL
   PERMISSIONS
-    FOR update, delete WHERE user_id = $auth.id
-    FOR select WHERE true;
+    FOR update, delete WHERE user = $auth.id
+    FOR create, select WHERE true;
 
 DEFINE FIELD user ON crops TYPE record<users> DEFAULT $auth.id;
 DEFINE FIELD name ON crops TYPE string;
@@ -24,5 +24,4 @@ DEFINE FIELD version ON crops TYPE string DEFAULT "Not Specified";
 DEFINE FIELD license ON crops TYPE string DEFAULT "Not Specified";
 DEFINE FIELD description ON crops TYPE string DEFAULT "No Description";
 
-USE NS farmer DB main;
 DEFINE USER view ON DATABASE PASSWORD 'view' ROLES VIEWER;
